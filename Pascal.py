0650a29b6df2e6ab55a40e2deaebeec0d5602451
Asorace-5034 <tafytoo@gmail.com>
2025-12-04 15:00:18 +0300

    Add files via upload

---
+++ Pascal.py
@@ -0,0 +1,18 @@
+def Pascal(n):
+    if n == 0:
+        return [1]
+    else:
+        row = [1]
+        previous_row = Pascal(n - 1)
+        for i in range(len(previous_row) - 1):
+            row.append(previous_row[i] + previous_row[i + 1])    
+        row.append(1)
+        return row
+
+def main():
+    n = int(input('Введите число, которое хотите превратить в треугольник Паскаля: '))
+    for i in range(n):
+        print(Pascal(i))
+
+if __name__ == "__main__":                              #Выполнение кода, если он запущен как самостоятельный файл
+    main()
